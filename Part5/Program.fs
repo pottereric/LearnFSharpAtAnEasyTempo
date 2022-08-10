df70ab08c2956ff6a1554fc6f609033be55bd442
Eric Potter <eric.b.potter@gmail.com>
2022-08-10 10:23:12 -0500

    Parts 5 and 6

---
+++ Part5/Program.fs
@@ -0,0 +1,11 @@
+﻿open FSharp.Data
+
+type usersData = CsvProvider< @"user_data.csv" >
+
+let myUsers = usersData.GetSample()
+let first = myUsers.Rows |> Seq.head
+
+printfn $"%s{first.FirstName} %s{first.LastName}"
+
+System.Console.ReadKey() |> ignore
+
