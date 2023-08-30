a55261379d1480346b6be098aa8d02030017158e
Eric Potter <ft@mailinator.com>
2023-08-30 09:28:21 -0500

    Catagorize All Spheres

--- Part2/Sphere.fsx
+++ Part2/Sphere.fsx
@@ -6,3 +6,10 @@ let catagorize r =
     if(volume < 100) then "small"
     else if(volume < 1000) then "medium"
     else "huge"
+
+let GetCatagories _ =
+    let inputValues = [1..10]
+    let outputs = List.map catagorize inputValues
+    List.iter (printfn "%s") outputs
+
+GetCatagories()
